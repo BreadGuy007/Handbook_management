WordPress/gutenberg

commit 5c0e81f18d57952c6199ff33047929206a28cd19

Latest posts block: Show error for password protected posts.

packages/block-library/src/latest-posts/index.php
@@ -135,6 +135,10 @@ function render_block_core_latest_posts( $attributes ) {
 
 			$trimmed_excerpt = get_the_excerpt( $post );
 
+			if ( post_password_required( $post ) ) {
+				$trimmed_excerpt = __( 'This content is password protected.' );
+			}
+
 			$list_items_markup .= sprintf(
 				'<div class="wp-block-latest-posts__post-excerpt">%1$s</div>',
 				$trimmed_excerpt
@@ -143,9 +147,16 @@ function render_block_core_latest_posts( $attributes ) {
 
 		if ( isset( $attributes['displayPostContent'] ) && $attributes['displayPostContent']
 			&& isset( $attributes['displayPostContentRadio'] ) && 'full_post' === $attributes['displayPostContentRadio'] ) {
-			$list_items_markup .= sprintf(
+
+				$post_content = wp_kses_post( html_entity_decode( $post->post_content, ENT_QUOTES, get_option( 'blog_charset' ) ) );
+
+				if ( post_password_required( $post ) ) {
+					$post_content = __( 'This content is password protected.' );
+				}
+
+				$list_items_markup .= sprintf(
 				'<div class="wp-block-latest-posts__post-full-content">%1$s</div>',
-				wp_kses_post( html_entity_decode( $post->post_content, ENT_QUOTES, get_option( 'blog_charset' ) ) )
+				$post_content
 			);
 		}
 

BreadGuy007/Handbook_management

commit 18ec7d0d2b6a280cab3676c6378a8e0142293cb0

Latest posts block: Fix linting errors introduced in  5c0e81f18d57952c6199ff33047929206a28cd19.

packages/block-library/src/latest-posts/index.php
@@ -155,9 +155,9 @@ function render_block_core_latest_posts( $attributes ) {
 				}
 
 				$list_items_markup .= sprintf(
-				'<div class="wp-block-latest-posts__post-full-content">%1$s</div>',
-				$post_content
-			);
+					'<div class="wp-block-latest-posts__post-full-content">%1$s</div>',
+					$post_content
+				);
 		}
 
 		$list_items_markup .= "</li>\n";